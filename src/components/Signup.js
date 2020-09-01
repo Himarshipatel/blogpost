75bf9c23e4e3f02fc0b5afaa562e53c2c487c541
Himarshipatel <62707959+Himarshipatel@users.noreply.github.com>
2020-09-01 14:34:33 +0530

    changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
-import { Form, FormGroup, Input, Container, Button } from "reactstrap";
+import { Form, FormGroup, Input, Container, Button, Col } from "reactstrap";
 import { yupResolver } from "@hookform/resolvers";
 import * as Yup from "yup";
 const schema = Yup.object().shape({
@@ -18,53 +18,59 @@ const Signup = () => {
   };
   return (
     <>
-      <Container>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <FormGroup>
-            <Controller
-              placeholder="Enter Name"
-              as={Input}
-              type="text"
-              ref={register}
-              control={control}
-              name="identifier"
-            />
-            {errors.identifier && (
-              <p className="text-danger">* {errors.identifier.message}</p>
-            )}
-          </FormGroup>
+      <Col className="home">
+        <Col className="form" sm="5">
+          <Col className="head">Sign Up</Col>
 
-          <FormGroup>
-            <Controller
-              placeholder="Enter Password"
-              as={Input}
-              type="password"
-              ref={register}
-              control={control}
-              name="password"
-            />
-            {errors.password && (
-              <p className="text-danger">* {errors.password.message}</p>
-            )}
-          </FormGroup>
-          <FormGroup>
-            <Controller
-              placeholder="Enter email"
-              as={Input}
-              type="text"
-              ref={register}
-              control={control}
-              name="email"
-            />
-            {errors.email && (
-              <p className="text-danger">* {errors.email.message}</p>
-            )}
-          </FormGroup>
-          <Button color="primary" type="submit" className="signbutton">
-            Sign up
-          </Button>
-        </Form>
-      </Container>
+          <Container>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <FormGroup>
+                <Controller
+                  placeholder="Enter Name"
+                  as={Input}
+                  type="text"
+                  ref={register}
+                  control={control}
+                  name="identifier"
+                />
+                {errors.identifier && (
+                  <p className="text-danger">* {errors.identifier.message}</p>
+                )}
+              </FormGroup>
+
+              <FormGroup>
+                <Controller
+                  placeholder="Enter Password"
+                  as={Input}
+                  type="password"
+                  ref={register}
+                  control={control}
+                  name="password"
+                />
+                {errors.password && (
+                  <p className="text-danger">* {errors.password.message}</p>
+                )}
+              </FormGroup>
+              <FormGroup>
+                <Controller
+                  placeholder="Enter email"
+                  as={Input}
+                  type="text"
+                  ref={register}
+                  control={control}
+                  name="email"
+                />
+                {errors.email && (
+                  <p className="text-danger">* {errors.email.message}</p>
+                )}
+              </FormGroup>
+              <Button color="primary" type="submit" className="signbutton">
+                Sign up
+              </Button>
+            </Form>
+          </Container>
+        </Col>
+      </Col>
     </>
   );
 };
